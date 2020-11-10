1158134e2ae1ec35477d6478c85aa65406c288aa
andika-big <74223810+andika-big@users.noreply.github.com>
2020-11-10 11:03:41 +0700

    Add files via upload

---
+++ InputOutputBig/src/com/dicoding/javafundamental/InputOutput/InputOutputFunction.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dicoding.javafundamental.InputOutput;
+
+import java.util.Scanner;
+
+public class InputOutputFunction {
+    
+  public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Program penjumalahan sangat sederhana");
+        System.out.print("Masukan Angka pertama : ");
+        int value = scanner.nextInt();
+        System.out.print("Masukan Angka kedua : ");
+        int anotherValue = scanner.nextInt();
+        int result = value + anotherValue;
+        System.out.println("Hasilnya adalah : " + result);
+    }  
+}
