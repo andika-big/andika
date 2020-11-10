54dfbc81cdab81a97a52c69cebb0c60e7aa576da
andika-big <74223810+andika-big@users.noreply.github.com>
2020-11-10 11:08:25 +0700

    Struktur Dasar

---
+++ StrukturDasarBig/src/com/dicoding/javafundamental/basic/main.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dicoding.javafundamental.basic;
+
+import com.dicoding.javafundamental.basic.keandaraan.pesawat;
+import com.dicoding.javafundamental.basic.musik.gong;
+
+/**
+ *
+ * @author User
+ */
+public class main {
+   
+    public static void main(String[] args) {
+        System.out.println("Hello Broo!");
+        
+        gong.bunyi();
+        
+        pesawat.jumlahBan();
+    }
+}
---
+++ StrukturDasarBig/src/com/dicoding/javafundamental/basic/musik/gong.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dicoding.javafundamental.basic.musik;
+
+/**
+ *
+ * @author User
+ */
+public class gong {
+    public static void bunyi(){
+        System.out.println("Duweng...");
+    }  
+}
