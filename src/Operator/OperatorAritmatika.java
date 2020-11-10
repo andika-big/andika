1145c16d8364d3510f62607bde8e5fdcbcd82f60
andika-big <dwicahyoandika9@gmail.com>
2020-11-10 10:40:22 +0700

    Pemrograman Dasar

---
+++ src/Operator/OperatorAritmatika.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Operator;
+
+/**
+ *
+ * @author User
+ */
+public class OperatorAritmatika {
+     public static void main(String[] args) {
+        System.out.println("Operasi Penjumlahan");
+        int hasilPenjumlahan = 4 + 2;
+        System.out.println("Hasil 4 + 2 = " + hasilPenjumlahan);
+        System.out.println();
+        
+        System.out.println("Operasi Pengurangan");
+        int hasilPengurangan = 9 - 6;
+        System.out.println("Hasil 9 - 6 = " + hasilPengurangan);
+        System.out.println();
+        
+        System.out.println("Operasi Pengalian");
+        int hasilPengalian = 9 * 7;
+        System.out.println("Hasil 9 * 7 = " + hasilPengalian);
+        System.out.println();
+        
+        System.out.println("Operasi Pembagian");
+        int hasilPembagian = 15 / 3;
+        System.out.println("Hasil 15 / 3 = " + hasilPembagian);
+        System.out.println();
+        
+        System.out.println("Operasi Habis bagi");
+        int hasilSisa = 9 % 3;
+        System.out.println("Hasil 9 % 3 = " + hasilSisa);
+        int hasilSisaLain = 7 % 2;
+        System.out.println("Hasil 7 % 2 = " + hasilSisaLain);
+        System.out.println();
+     }
+}
